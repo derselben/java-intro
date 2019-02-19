fcb2c730f6bb89125ad918957f281caaf7712b68
derselben <derselben@gmail.com>
2019-02-19 16:48:14 +0200

    data types were added

--- src/lesson01/HelloWorld.java
+++ src/lesson01/HelloWorld.java
@@ -4,5 +4,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!\r\nAgain!");
+        short s = 9999;
+        long l = 10046565768584745L;
+        int i = 700;
+        float f = 4575f;
+        double d = 56778;
+        System.out.println(s+"\n"+l+"\n"+i+"\n"+f+"\n"+d);
     }
 }
