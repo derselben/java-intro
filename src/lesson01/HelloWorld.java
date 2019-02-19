22e78aa8490846bf030e01e920335529d8e42640
derselben <derselben@gmail.com>
2019-02-19 16:04:12 +0200

    one more line added with message

--- src/lesson01/HelloWorld.java
+++ src/lesson01/HelloWorld.java
@@ -3,5 +3,6 @@ package lesson01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!\r\nAgain!");
     }
 }
