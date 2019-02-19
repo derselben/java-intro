983d3b8bb94794f3a8c9777ee1df4dc875edac75
derselben <derselben@gmail.com>
2019-02-19 18:52:51 +0200

    project structure was added

---
+++ src/lesson01/Test.java
@@ -0,0 +1,10 @@
+package lesson01;
+
+    public class Test{
+
+        public static void main(String[] args){
+            Integer x = 5; // упаковка int в целочисленный объект (Integer)
+            x =  x + 10;   // распаковка Integer в int
+            System.out.println(x.toString());
+        }
+    }
