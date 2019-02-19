0294865830d0c8d54128db7b29b86cfe65630ab8
derselben <derselben@gmail.com>
2019-02-19 18:18:22 +0200

    missing data types were added

--- src/lesson01/HelloWorld.java
+++ src/lesson01/HelloWorld.java
@@ -4,11 +4,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!\r\nAgain!");
+        byte b = 127;
         short s = 9999;
-        long l = 10046565768584745L;
+        char c = 'c';
         int i = 700;
+        long l = 10046565768584745L;
         float f = 4575f;
         double d = 56778;
-        System.out.println(s+"\n"+l+"\n"+i+"\n"+f+"\n"+d);
+        boolean o = true;
+        System.out.println(s+"\n"+l+"\n"+i+"\n"+f+"\n"+d+"\n"+b+"\n"+c+"\n"+o);
     }
 }
