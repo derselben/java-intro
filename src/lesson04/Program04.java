cb443b15bc94050df28ed04c8d8dfa2d01774193
derselben <derselben@gmail.com>
2019-03-01 10:09:01 +0200

    lesson04 homework

--- src/lesson04/Program04.java
+++ src/lesson04/Program04.java
@@ -1,5 +1,9 @@
 package lesson04;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Program04 {
     public static void main(String[] args) {
 
@@ -8,6 +12,39 @@ public class Program04 {
 
 
         /** HOME WORK */
+
+        ArrayList <String> arr = new ArrayList<>();
+        // add 9 strings
+        arr.add("Yemen");
+        arr.add("Saint Pierre and Miquelon");
+        arr.add("Bolivia");
+        arr.add("Italy");
+        arr.add("Switzerland");
+        arr.add("Dominica");
+        arr.add("Barbados");
+        arr.add("Vanuatu");
+        arr.add("Kuwait");
+
+        Random r = new Random();
+        int i = r.nextInt(arr.size());
+        String v = arr.get(i);
+
+        // remove 1 value
+        System.out.println("***** Removing '" + v +"' from the Collection *****");
+        arr.remove(v);
+
+        // check if value is not in collection anymore and print the result
+        if (!arr.contains(v)) System.out.println("***** '" + v + "' is not in the Collection anymore! *****\n");
+
+        // print collection before sort
+        for (String s: arr) System.out.println(s);
+
+        // sort values in the collection
+        Collections.sort(arr);
+        System.out.println("\n***** Collection was sorted! *****");
+
+        // print collection after sort
+        for (String s: arr) System.out.println(s);
 
     }
 }
